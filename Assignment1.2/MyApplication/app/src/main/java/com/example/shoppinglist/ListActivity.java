35a23b8bdb3f3db543e731d9fa3489f2499718c2
Yosri Jerbi <yosri.jerbi@hotmail.com>
2016-02-02 16:18:39 +0100

    Step 2.2: Get the ListView.

--- Assignment1.2/MyApplication/app/src/main/java/com/example/shoppinglist/ListActivity.java
+++ Assignment1.2/MyApplication/app/src/main/java/com/example/shoppinglist/ListActivity.java
@@ -8,9 +8,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
-
+    //views
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +20,8 @@ public class ListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+        //Initialize the views
+        listView = (ListView) findViewById(R.id.listView);
     }
 
     @Override
