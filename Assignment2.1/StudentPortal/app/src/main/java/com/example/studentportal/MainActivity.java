48d621d7d3ec38dba50ecbfef458c557d2704bbf
Yosri Jerbi <yosri.jerbi@hotmail.com>
2016-02-08 21:31:49 +0100

    Step 4: Implement action

--- Assignment2.1/StudentPortal/app/src/main/java/com/example/studentportal/MainActivity.java
+++ Assignment2.1/StudentPortal/app/src/main/java/com/example/studentportal/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.studentportal;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,52 +10,64 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class MainActivity extends AppCompatActivity {
-        //Adapter and ArrayList
-        private ArrayAdapter<ListItem> adapter;
-        //Views
-        private ListView listView;
-        //ListItem
-        private List<ListItem> items;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-            setSupportActionBar(toolbar);
+    //Adapter and ArrayList
+    private ArrayAdapter<ListItem> adapter;
+    //Views
+    private ListView listView;
+    //ListItem
+    private List<ListItem> items;
 
-            //Initialize the views
-            listView = (ListView) findViewById(R.id.listView);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-            //Initialize item list
-            items = new ArrayList<ListItem>();
+        //Initialize the views
+        listView = (ListView) findViewById(R.id.listView);
 
-            //Adding items to the list
-            items.add(new ListItem("VLO", "https://home.informatica.hva.nl/vlo/"));
-            items.add(new ListItem("DMCI", "https://ict.dmci.hva.nl/"));
-            items.add(new ListItem("Rooster", "https://rooster.hva.nl/"));
-            items.add(new ListItem("Resultaten", "https://resultaten.hva.nl/"));
+        //Initialize item list
+        items = new ArrayList<ListItem>();
 
-            adapter = new ArrayAdapter<ListItem>(this, android.R.layout.simple_list_item_1, items);
-            //Set the newly created adapter as the adapter for the listview
-            listView.setAdapter(adapter);
+        //Adding items to the list
+        items.add(new ListItem("VLO", "https://home.informatica.hva.nl/vlo/"));
+        items.add(new ListItem("DMCI", "https://ict.dmci.hva.nl/"));
+        items.add(new ListItem("Rooster", "https://rooster.hva.nl/"));
+        items.add(new ListItem("Resultaten", "https://resultaten.hva.nl/"));
 
-            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                }
-            });
-        }
+        adapter = new ArrayAdapter<ListItem>(this, android.R.layout.simple_list_item_1, items);
+        //Set the newly created adapter as the adapter for the listview
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View listItem, int position, long id) {
+                ListItem clickedItem = (ListItem) parent.getItemAtPosition(position);
+                Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(clickedItem.getUrl()));
+                startActivity(myIntent);
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
